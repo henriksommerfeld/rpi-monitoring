9d000fbb36237610608c32c6731d321352d65113
Henrik Sommerfeld <henrik@sommerfeld.nu>
2016-09-03 17:58:31 +0200

    Changed wait handling from sleep to using time

--- sensor_read.py
+++ sensor_read.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 import time
 import os
+import threading
 from sense_hat import SenseHat
 from led_message import LedStateMessage
-SEND_DATA_INTERVAL_SECONDS = 10
+SEND_DATA_INTERVAL_SECONDS = 10.0
+DEBUG = True
 
-def debug(temp, pressure, humidity):
+def debug_reading(temp, pressure, humidity):
 	t = round(temp, 1)
 	p = round(pressure, 1)
 	h = round(humidity, 1)
 	msg = "Temperature: {0} C. Pressure: {1} mBAR. Humidity: {2}%".format(t, p, h)
-	print(msg)
+	debug(msg)
+
+def debug(message):
+	if DEBUG:
+		print(message)
 
 def get_cpu_temperature():  
 	full_temperature_string = os.popen('vcgencmd measure_temp').readline()  
@@ -23,22 +29,30 @@ def get_ambient_temperature():
 	ambient_temp = temperature_from_sensor - ((cpu_temp - temperature_from_sensor)/ 1.5)
 	return ambient_temp
 
-def send_data(temp, pressure, humidity):
+def send_data(temp, pressure, humidity, last_time_sent):
 	try:
-		time.sleep(SEND_DATA_INTERVAL_SECONDS)
-		# todo: send data 
-		screen.show_ok_message()
-	except Exception,e:
-		print str(e)
-		screen.show_warning_message()	
+		if time.time() - last_time_sent > SEND_DATA_INTERVAL_SECONDS:
+			debug("trying to send data...")
+			# todo: send data
+			screen.show_ok_message()
+			debug("sent data")
+			last_time_sent = time.time()
+	except Exception as e:
+		last_time_sent = time.time()
+		print(e)
+		screen.show_warning_message()
+	finally:
+		return last_time_sent
 
 def read_sensors():
+	last_time_sent = 0.0
+
 	while True:
 		pressure = sense.get_pressure()
 		humidity = sense.get_humidity()
 		temperature = get_ambient_temperature()
-		debug(temperature, pressure, humidity)
-		send_data(temperature, pressure, humidity)		
+		debug_reading(temperature, pressure, humidity)
+		last_time_sent = send_data(temperature, pressure, humidity, last_time_sent)
 		time.sleep(1)
 
 def main():
@@ -46,8 +60,8 @@ def main():
 		read_sensors()
 	except(KeyboardInterrupt, SystemExit):
 		screen.clear()
-	except Exception,e:
-		print str(e)
+	except Exception as e:
+		print(e)
 		screen.show_error_message()
 
 sense = SenseHat()  
